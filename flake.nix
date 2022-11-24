82ee54fe32a42c16f30251d3399dcf9674ba1e61
Jon <dyercode@users.noreply.github.com>
2022-11-24 01:15:59 -0500

    switch to flake.
    no pulling down all jdk.

--- flake.nix
+++ flake.nix
@@ -4,14 +4,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    dev.url = "github:dyercode/dev";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, dev }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ sbt ];
+          nativeBuildInputs = with pkgs; [
+            sbt
+            dev.defaultPackage.${system}
+          ];
 
           shellHook = ''
             export PI4J_PLATFORM=Simulated
